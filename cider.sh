2819873b9a8671b7276867442ef36a41b7c8df98
Mikhail Shchekotov <mish@boozt.com>
2023-05-05 11:40:42 +0200

    Add <center> alignment

--- cider.sh
+++ cider.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CIDER_version="3.0.1"
+CIDER_version="3.1.1"
 
 # a cellar is the place where a cider comes from
 CIDER_cellar="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
