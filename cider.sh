269c4ba7622af5c0b28836101c0b9f2e59cfe8e9
Mikhail Shchekotov <torunar@gmail.com>
2024-02-29 10:49:46 +0100

    Improve gallery column distribution

--- cider.sh
+++ cider.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CIDER_version="4.0.1"
+CIDER_version="4.0.2"
 
 # a cellar is the place where a cider comes from
 CIDER_cellar="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
